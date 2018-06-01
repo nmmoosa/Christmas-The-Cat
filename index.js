0796967af6a99d833f4b5b777dea7c87eed320c1
nmmoosa <37936636+nmmoosa@users.noreply.github.com>
2018-06-02 01:22:18 +0800

    Newer Updates

---
+++ index.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+
+    $("#submit").click(function() {
+        event.preventDefault();
+
+        var formData = $("form").serializeArray();
+        console.log(formData);
+
+        var nameInput = $("#name").val();
+        console.log("Name Input: ", nameInput);
+    })
+    
+})
